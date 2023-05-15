57138034d2a753fa1b34f98fda847a21553244f7
Muhammad Dzulfiqar Rafli Anwar <rafli.anwar2001@gmail.com>
2023-05-16 00:28:08 +0700

    Member Dashboard
    movie list

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\TransactionController;
 use App\Http\Controllers\Admin\LoginController;
 use App\Http\Controllers\Member\RegisterController;
 use App\Http\Controllers\Member\LoginController as MemberLoginController;
+use App\Http\Controllers\Member\DashboardController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -45,6 +46,10 @@ Route::post('/register', [RegisterController::class, 'store'])->name('member.reg
 
 Route::get('/login', [MemberLoginController::class, 'index'])->name('member.login');
 Route::post('/login', [MemberLoginController::class, 'auth'])->name('member.login.auth');
+
+Route::group(['prefix' => 'member'], function(){
+    Route::get('/', [DashboardController::class, 'index'])->name('member.dashboard');
+});
 
 
 
